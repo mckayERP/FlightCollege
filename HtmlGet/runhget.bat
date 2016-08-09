f74e18330548cd62d2072ad27ee6ea51f31ad7c4
mckayERP <michael.mckay@mckayERP.com>
2016-08-09 08:37:59 -0400

    Remove HTMLGet and GLRec code from repository as it is no longer required.

--- HtmlGet/runhget.bat
+++
@@ -1,6 +0,0 @@
-cd bin
-SET CLASSPATH=c:\dev\projects\htmlget\bin\WebQueryServer.jar;c:\dev\projects\htmlget\bin\gnu-regexp-1.1.4.jar;c:\dev\projects\htmlget\bin\libreadline-java.jar
-
-java  -classpath %CLASSPATH% HtmlGet.HtmlGet
-
-cd ..
